bdffe6390c18d17c887c755ea3f633fa9e2b5ed1
Serhii Kotsiuba <s-p-ko@ukr.net>
2022-08-30 08:40:19 +0300

    Merge conflicts

--- functions.php
+++ functions.php
@@ -5,3 +5,8 @@ function get_profile($id)
     $profile = get_user_by(‘id’, $id);
     return $profile;
 }
+
+function get_user_meta($id, $key)
+{
+    return get_metadata('user', $user_id, $key, $single);
+}
